bf3038b7df74a9b4f0a6f1e763fc8cf936796748
Aleksei Briushinin <a.briushinin@novotorica.com>
2026-01-26 15:35:13 +0300

    Add print mode

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import time
+from enum import Enum
 
 from fpdf import FPDF
 from openpyxl import load_workbook
@@ -34,6 +36,19 @@ BOTTOM_MARGIN = 10
 FONT_SIZE = 8
 
 
+class PrintMode(Enum):
+    one_per_page = 'one_per_page'
+    multiple_fit = 'multiple_fit'
+
+    @property
+    def name(self) -> str:
+        names = {
+            self.one_per_page.value: 'Одна квитанция на страницу',
+            self.multiple_fit.value: 'Сколько влезет квитанций на страницу',
+        }
+        return names[self.value]
+
+
 def resource_path(relative):
     """Для получения доступа к ресурсам.
 
@@ -57,96 +72,7 @@ def get_height_multi_cell(font_file, font_size, text: str) -> int:
     return int(end_y - start_y)
 
 
-def main():
-    try:
-        wb = load_workbook(PATH_TO_PEOPLE, read_only=True, data_only=True)
-    except FileNotFoundError:
-        file_people = FILE_NAME_PEOPLE.split('.')
-        raise FileNotFoundError(f'Проверьте что excel файл называется "{file_people[0]}" '
-                                f'и имеет расширение "{file_people[1]}"')
-    font_filename = 'DejaVuSansCondensed.ttf'
-    font_file = resource_path(font_filename)
-    pdf = FPDF()
-    pdf.add_font('DejaVu', '', font_file, uni=True)
-    pdf.set_top_margin(TOP_MARGIN)
-    pdf.set_left_margin(LEFT_MARGIN)
-    pdf.set_auto_page_break(True, BOTTOM_MARGIN)
-
-    customization = get_customization(wb)
-    people = get_people(wb).values()
-    pdf.add_page()
-    remaining_height = PAGE_HEIGHT
-    height_bottom_line = get_height_multi_cell(font_file, FONT_SIZE, customization['Последняя строка'])
-
-    for i, person in enumerate(people):
-        # если нет задолженности - пропускаем
-        if person.get_sum_debt() == 0:
-            continue
-        # определим, надо ли добавить новую страницу
-        person_height = person.get_height(height_bottom_line)
-        if remaining_height == PAGE_HEIGHT:
-            remaining_height -= person_height
-        elif remaining_height >= person_height + HEIGHT * 2:
-            pdf.cell(PAGE_WIDTH, HEIGHT, '_ ' * 86, ln=1, align='C')
-            pdf.cell(PAGE_WIDTH, HEIGHT, ln=1)
-            remaining_height -= person_height + HEIGHT * 2
-        else:
-            pdf.add_page()
-            remaining_height = PAGE_HEIGHT - person_height
-
-        pdf.set_font('DejaVu', '', FONT_SIZE)
-        pdf.cell(PAGE_WIDTH, HEIGHT, str(customization['Название']), 0, 1, 'C', )
-        pdf.cell(PAGE_WIDTH, HEIGHT, str(customization['Заголовок']), 0, 1, 'C', )
-        date_of_receipt_formation = customization['Дата формирования'].strftime(DATE_FORMAT)
-        pdf.cell(PAGE_WIDTH, HEIGHT, f'Сформировано на {date_of_receipt_formation}г.', 0, 1, 'L', )
-        pdf.cell(PAGE_WIDTH, HEIGHT * 4, '', 1, 0, 'C', )
-        pdf.cell(-PAGE_WIDTH)
-        pdf.cell(PAGE_WIDTH, HEIGHT, str(customization['Получатель платежа 1 строка']), 0, 1, 'L', )
-        pdf.cell(PAGE_WIDTH, HEIGHT, str(customization['Получатель платежа 2 строка']), 0, 1, 'L', )
-        pdf.cell(PAGE_WIDTH, HEIGHT, str(customization['Получатель платежа 3 строка']), 0, 1, 'L', )
-        pdf.cell(PAGE_WIDTH, HEIGHT, str(customization['Получатель платежа 4 строка']), 0, 1, 'L', )
-        pdf.cell(PAGE_WIDTH, HEIGHT, f'Плательщик: {person.name} ({person.phone})', 0, 1, 'L', )
-        pdf.cell(PAGE_WIDTH, HEIGHT, f'Адрес: {person.address}', 0, 1, 'L', )
-        pdf.cell(PAGE_WIDTH, HEIGHT, '', 0, 1, 'L', )
-
-        for box in person.boxes:
-            pdf.cell(PAGE_WIDTH, HEIGHT, f'Гараж № {box.number}', 0, 1, 'L', )
-            pdf.cell(CELL_WIDTH * 2, HEIGHT, 'Начислено членских взносов', 1, 0, 'C', )
-            pdf.cell(CELL_WIDTH, HEIGHT, 'Оплачено', 1, 0, 'C', )
-            pdf.cell(CELL_WIDTH * 2, HEIGHT, 'Задолженность', 1, 1, 'C', )
-            pdf.cell(CELL_WIDTH, HEIGHT, 'Год', 1, 0, 'C', )
-            pdf.cell(CELL_WIDTH, HEIGHT, 'Сумма', 1, 0, 'C', )
-            pdf.cell(CELL_WIDTH, HEIGHT, 'Сумма', 1, 0, 'C', )
-            pdf.cell(CELL_WIDTH, HEIGHT, 'Срок оплаты', 1, 0, 'C', )
-            pdf.cell(CELL_WIDTH, HEIGHT, 'Сумма', 1, 1, 'C', )
-
-            for accrual in box.accruals:
-                pdf.cell(CELL_WIDTH, HEIGHT, str(accrual.year), 1, 0, 'C', )
-                pdf.cell(CELL_WIDTH, HEIGHT, str(accrual.accrued), 1, 0, 'C', )
-
-                if accrual.payment_amount == 0:
-                    payment_amount = ''
-                else:
-                    payment_amount = str(accrual.payment_amount)
-                pdf.cell(CELL_WIDTH, HEIGHT, payment_amount, 1, 0, 'C', )
-                pdf.cell(CELL_WIDTH, HEIGHT, accrual.due_date, 1, 0, 'C', )
-                pdf.cell(CELL_WIDTH, HEIGHT, str(accrual.debt), 1, 1, 'C', )
-            pdf.cell(20, h=HEIGHT, ln=1)
-
-        pdf.cell(PAGE_WIDTH, HEIGHT, f'Всего начислено: {person.get_sum_accrued()} руб.', 0, 1, 'R', )
-        sum_debt = str(person.get_sum_debt())
-        pdf.cell(PAGE_WIDTH, HEIGHT, f'Всего задолженность: {sum_debt} руб.', 0, 1, 'R', )
-
-        pdf.cell(20, h=HEIGHT, ln=1)
-        pdf.multi_cell(PAGE_WIDTH, HEIGHT, customization['Последняя строка'], border=0, align='L')
-
-    try:
-        pdf.output(os.path.join(BASE_DIRECTORY, 'Квитанции.pdf'), 'F')
-    except PermissionError:
-        raise PermissionError('Проверьте, что закрыли pdf-файл "Квитанции" перед созданием нового')
-
-
-def get_people(wb):
+def get_people(wb) -> dict:
     first_row = 3  # первая строка с людьми
     try:
         ws = wb[SHEET_NAME_PEOPLE]
@@ -252,7 +178,7 @@ def get_people(wb):
     return people
 
 
-def get_customization(wb):
+def get_customization(wb) -> dict:
     keys = ['Название', 'Заголовок', 'Дата формирования', 'Получатель платежа 1 строка',
             'Получатель платежа 2 строка', 'Получатель платежа 3 строка', 'Получатель платежа 4 строка',
             'Последняя строка', ]
@@ -282,9 +208,147 @@ def get_customization(wb):
     return customization
 
 
+def get_pdf(people: dict, customization: dict, font_file, print_mode: PrintMode) -> FPDF:
+    pdf = FPDF()
+    pdf.add_font('DejaVu', '', font_file, uni=True)
+    pdf.set_top_margin(TOP_MARGIN)
+    pdf.set_left_margin(LEFT_MARGIN)
+    pdf.set_auto_page_break(True, BOTTOM_MARGIN)
+
+    pdf.add_page()
+    remaining_height = PAGE_HEIGHT
+    height_bottom_line = get_height_multi_cell(font_file, FONT_SIZE, customization['Последняя строка'])
+
+    for i, person in enumerate(people.values()):
+        # если нет задолженности - пропускаем
+        if person.get_sum_debt() == 0:
+            continue
+
+        # Определим, надо ли добавить новую страницу
+        if print_mode == PrintMode.multiple_fit:
+            person_height = person.get_height(height_bottom_line)
+            if remaining_height >= PAGE_HEIGHT:
+                remaining_height -= person_height
+            elif remaining_height >= person_height + HEIGHT * 2:
+                pdf.cell(PAGE_WIDTH, HEIGHT, '_ ' * 86, ln=1, align='C')
+                pdf.cell(PAGE_WIDTH, HEIGHT, ln=1)
+                remaining_height -= person_height + HEIGHT * 2
+            else:
+                pdf.add_page()
+                remaining_height = PAGE_HEIGHT - person_height
+        elif print_mode == PrintMode.one_per_page:
+            person_height = person.get_height(height_bottom_line)
+            if remaining_height >= PAGE_HEIGHT:
+                remaining_height -= person_height
+            else:
+                pdf.add_page()
+                remaining_height = PAGE_HEIGHT - person_height
+        else:
+            raise ValueError(f'Unknown print_mode: {print_mode}')
+
+        pdf.set_font('DejaVu', '', FONT_SIZE)
+        pdf.cell(PAGE_WIDTH, HEIGHT, str(customization['Название']), 0, 1, 'C', )
+        pdf.cell(PAGE_WIDTH, HEIGHT, str(customization['Заголовок']), 0, 1, 'C', )
+        date_of_receipt_formation = customization['Дата формирования'].strftime(DATE_FORMAT)
+        pdf.cell(PAGE_WIDTH, HEIGHT, f'Сформировано на {date_of_receipt_formation}г.', 0, 1, 'L', )
+        pdf.cell(PAGE_WIDTH, HEIGHT * 4, '', 1, 0, 'C', )
+        pdf.cell(-PAGE_WIDTH)
+        pdf.cell(PAGE_WIDTH, HEIGHT, str(customization['Получатель платежа 1 строка']), 0, 1, 'L', )
+        pdf.cell(PAGE_WIDTH, HEIGHT, str(customization['Получатель платежа 2 строка']), 0, 1, 'L', )
+        pdf.cell(PAGE_WIDTH, HEIGHT, str(customization['Получатель платежа 3 строка']), 0, 1, 'L', )
+        pdf.cell(PAGE_WIDTH, HEIGHT, str(customization['Получатель платежа 4 строка']), 0, 1, 'L', )
+        pdf.cell(PAGE_WIDTH, HEIGHT, f'Плательщик: {person.name} ({person.phone})', 0, 1, 'L', )
+        pdf.cell(PAGE_WIDTH, HEIGHT, f'Адрес: {person.address}', 0, 1, 'L', )
+        pdf.cell(PAGE_WIDTH, HEIGHT, '', 0, 1, 'L', )
+
+        for box in person.boxes:
+            pdf.cell(PAGE_WIDTH, HEIGHT, f'Гараж № {box.number}', 0, 1, 'L', )
+            pdf.cell(CELL_WIDTH * 2, HEIGHT, 'Начислено членских взносов', 1, 0, 'C', )
+            pdf.cell(CELL_WIDTH, HEIGHT, 'Оплачено', 1, 0, 'C', )
+            pdf.cell(CELL_WIDTH * 2, HEIGHT, 'Задолженность', 1, 1, 'C', )
+            pdf.cell(CELL_WIDTH, HEIGHT, 'Год', 1, 0, 'C', )
+            pdf.cell(CELL_WIDTH, HEIGHT, 'Сумма', 1, 0, 'C', )
+            pdf.cell(CELL_WIDTH, HEIGHT, 'Сумма', 1, 0, 'C', )
+            pdf.cell(CELL_WIDTH, HEIGHT, 'Срок оплаты', 1, 0, 'C', )
+            pdf.cell(CELL_WIDTH, HEIGHT, 'Сумма', 1, 1, 'C', )
+
+            for accrual in box.accruals:
+                pdf.cell(CELL_WIDTH, HEIGHT, str(accrual.year), 1, 0, 'C', )
+                pdf.cell(CELL_WIDTH, HEIGHT, str(accrual.accrued), 1, 0, 'C', )
+
+                if accrual.payment_amount == 0:
+                    payment_amount = ''
+                else:
+                    payment_amount = str(accrual.payment_amount)
+                pdf.cell(CELL_WIDTH, HEIGHT, payment_amount, 1, 0, 'C', )
+                pdf.cell(CELL_WIDTH, HEIGHT, accrual.due_date, 1, 0, 'C', )
+                pdf.cell(CELL_WIDTH, HEIGHT, str(accrual.debt), 1, 1, 'C', )
+            pdf.cell(20, h=HEIGHT, ln=1)
+
+        pdf.cell(PAGE_WIDTH, HEIGHT, f'Всего начислено: {person.get_sum_accrued()} руб.', 0, 1, 'R', )
+        sum_debt = str(person.get_sum_debt())
+        pdf.cell(PAGE_WIDTH, HEIGHT, f'Всего задолженность: {sum_debt} руб.', 0, 1, 'R', )
+
+        pdf.cell(20, h=HEIGHT, ln=1)
+        pdf.multi_cell(PAGE_WIDTH, HEIGHT, customization['Последняя строка'], border=0, align='L')
+
+    return pdf
+
+
+def select_print_mode() -> PrintMode:
+    """Prompt user to select print mode."""
+    print('\nВыберите режим печати:')
+    print(f'1. {PrintMode.multiple_fit.name} (по умолчанию)')
+    print(f'2. {PrintMode.one_per_page.name}\n')
+
+    print_mode = None
+
+    while print_mode is None:
+        choice = input('Введите номер режима (1 или 2): ').strip()
+        if choice in ('1', ''):
+            print_mode = PrintMode.multiple_fit
+        elif choice == '2':
+            print_mode =  PrintMode.one_per_page
+        else:
+            print('Ошибка: введите 1 или 2')
+
+    print(f'Выбран режим "{print_mode.name}"\n')
+    time.sleep(2)
+    return print_mode
+
+
+def main(print_mode: PrintMode):
+    print('Генерация квитанций...')
+    try:
+        wb = load_workbook(PATH_TO_PEOPLE, read_only=True, data_only=True)
+    except FileNotFoundError:
+        file_name, extension = FILE_NAME_PEOPLE.split('.')
+        raise FileNotFoundError(f'Проверьте что excel файл называется "{file_name}" '
+                                f'и имеет расширение "{extension}"')
+
+    font_filename = 'DejaVuSansCondensed.ttf'
+    font_file = resource_path(font_filename)
+    customization = get_customization(wb)
+    people = get_people(wb)
+    pdf = get_pdf(
+        people=people,
+        customization=customization,
+        font_file=font_file,
+        print_mode=print_mode,
+    )
+
+    try:
+        pdf.output(os.path.join(BASE_DIRECTORY, 'Квитанции.pdf'), 'F')
+    except PermissionError:
+        raise PermissionError('Проверьте, что закрыли pdf-файл "Квитанции" перед созданием нового')
+
+    print('Квитанции сгенерированы успешно')
+
+
 if __name__ == '__main__':
     try:
-        main()
+        selected_print_mode = select_print_mode()
+        main(print_mode=selected_print_mode)
     except Exception as e:
         print('В ходе работы программы возникла ошибка:')
         print(e)
